27423b7fd1b548d6f83024f5a017a11cefcf8f47
Will Yeadon <chvyalthan@gmail.com>
2022-12-15 11:12:38 +0000

    dommies

--- ctci/chpt6/problem-6-3.py
+++ ctci/chpt6/problem-6-3.py
@@ -1,7 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Dec 15 10:40:26 2022
+# Chess board with two corner squares removed
+# can you lay 31 dominoes if each take up 2 adjacent
+# squares?
 
-@author: chvya
-"""
 
+# Answer is no as 32w/30b or 30w/32b means they'll
+# always be either 2w or 2b left over and need
+# black and white adjacent to lay dominoe down.
