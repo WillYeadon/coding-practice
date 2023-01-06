61cdc7f5e4884a2ad8138f1a7298d7a4d5a6e41f
Will Yeadon <chvyalthan@gmail.com>
2023-01-06 21:23:33 +0000

    9-2

---
+++ ctci/chpt9/problem-9-2.py
@@ -0,0 +1,9 @@
+# For a social network (FB or LinkedIn) you'd 
+# assume a graph like DB would apt w/ a limit on
+# how many connections any one person could have
+# say 5000. It would be slightly lopsided for read
+# over write but not as dramatically as something
+# like Youtube or Twitter.
+
+# Shortest path would be a travelling salesman like
+# algorithm such as Dijkstra's .
