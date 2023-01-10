3352166a8204699de4a22c96f9a168de6ed394d1
Will Yeadon <chvyalthan@gmail.com>
2023-01-10 13:57:59 +0000

    7-4

---
+++ ctci/chpt7/problem-7-4.py
@@ -0,0 +1,59 @@
+from random import randint
+# OOP carpark
+
+class vehicle():
+    def __init__(self, size):
+        self.size = size
+        
+    def printSize(self):
+        print(self.size)
+
+class car(vehicle):
+    def __init__(self, size, make, model):
+        super().__init__(size)
+        self.make = make
+        self.model = model
+    
+    def printCar(self):
+        print(self.make,
+              self.model)
+        
+class carPark():
+    def __init__(self, x, y):
+        self.length = x
+        self.width = y
+        print(x, y)
+        self.spaces = self.createSpace(x, y)
+    
+    def createSpace(self, x, y):
+        inner = ['Empty'] * self.length
+        hold = [inner] * self.width
+
+        hold = [['Empty', 'Empty', 'Empty', 'Empty'],
+                ['Empty', 'Empty', 'Empty', 'Empty'],
+                ['Empty', 'Empty', 'Empty', 'Empty']]
+       
+        return hold
+            
+    def printSpaces(self):
+        for i in self.spaces:
+            print(i)
+            
+    def park(self, car):
+        parked = False
+        
+        while not parked:      
+            y = randint(0, self.length - 1)
+            x = randint(0, self.width - 1)
+
+            if self.spaces[x][y] == 'Empty':
+                self.spaces[x][y] =\
+                    car.make + ' ' + car.model
+                parked = True
+
+boj = car('Small', 'Vaxhaull', 'Corsa')
+
+pods = carPark(4, 3)
+pods.printSpaces()
+pods.park(boj)
+pods.printSpaces()
