35fedeec405c86946a9e7d8484f757890282078c
Will Yeadon <chvyalthan@gmail.com>
2023-01-11 09:58:43 +0000

    6-5

---
+++ ctci/chpt6/problem-6-5.py
@@ -0,0 +1,9 @@
+# 5 pint and 3 pint jugs, get four pints
+
+# Fill 5 pint jug up
+# Pour the 5 pint into 3 pint jug
+# Sling the 3 pints
+# Remaining 2 pints from 5 pinter into the 3 pint jug
+# Fill 5 pint jug up
+# Pour the 5 pint into 3 pint jug
+# You're left with 4 pints int eh 5 pint jug (and 3 in the 3 pint)
