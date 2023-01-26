8c6e5a20a21b2a90fe9ec4ce74eda71c929fe71a
Will Yeadon <chvyalthan@gmail.com>
2023-01-26 19:48:08 +0000

    10

---
+++ ctci/chpt10/problem-10-2.py
@@ -0,0 +1,7 @@
+anos = ['pass', 'loop', 'aged', 'pool', 'care',
+        'cake', 'rake', 'race', 'deck', 'hoop',
+        'bake', 'beak', 'sire', 'liar', 'hire']
+
+# Can't put in print, need separate line here.
+#anos.sort()
+print(sorted(anos))
