c82f9ece331ffad56b11d345c492ffefb764daca
Will Yeadon <chvyalthan@gmail.com>
2023-01-26 19:26:32 +0000

    11

--- ctci/chpt11/problem-11-1.py
+++ ctci/chpt11/problem-11-1.py
@@ -1,1 +1,10 @@
-pass
+# unsigned int i;
+# for (i = 100; i >= 0; i--)
+#    printf("%d\n", i);
+
+# You're going to end up with an off by one error as
+# the int is unsigned and so can never be < 0 thus
+# the condition i >= 0 will always be true and the 
+# loop will execute indefinitely.
+
+# Should also use %u instead of %d
