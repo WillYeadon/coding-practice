18e46279e4f0e7befa906f82d5c0bc1cd98c42e4
Will Yeadon <chvyalthan@gmail.com>
2023-01-28 20:06:56 +0000

    9

---
+++ ctci/chpt9/problem-9-3.py
@@ -0,0 +1,12 @@
+# Web crawler
+
+# Answer was quite vague here.
+
+# Got to a page and record various details about it
+# and hash the URL
+
+# Check database about whether this hashed URL has been 
+# crawled recently
+
+# If it hasn't been crawled recently then record page 
+# & its links
