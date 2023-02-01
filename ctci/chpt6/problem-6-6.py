0ae29d8e4317f79e6e342ca25d5ae42fc5934558
Will Yeadon <chvyalthan@gmail.com>
2023-02-01 13:25:46 +0000

    6-6

---
+++ ctci/chpt6/problem-6-6.py
@@ -0,0 +1,31 @@
+# Blue eyed island
+
+# There is an island full of N people where someone comes
+# along and says all blue eyed people must leave the island.
+# There is a boat that transports people off every evening
+# at 8 pm. Everyone can see each others eye colour but not
+# their own. Critically, IN NO WAY can they communicate what
+# someone else's eyecolour is to them. This includes saying
+# 'you should leave' or 'You don't have brown, green, red,
+# etc... eye colour (thus you must have...)'. The question 
+# doesn't specify this. 
+
+# How many days will it take for all the blue eyed people
+# to leave the island if atleast one person on the island
+# has blue eyes?
+
+# The answer here is that on the first day if you only see
+# say brown coloured eyes and not any blue then you must
+# be the one who has blue eyes so you leave.
+
+# If no on leave on the first day and you see one other 
+# person with blue eyes you know it must be you both have
+# blue eyes so two (including you) will leave on the second
+# night.
+
+# On the third night if no one has left but you see two 
+# others have blue eyes then it must be you three only so
+# you leave on the third night.
+
+# Hence.... for B people with blue eyes out of N will take
+# B nights to figure out.
