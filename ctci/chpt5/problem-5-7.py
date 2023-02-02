a528ac5bac150017414e8f8c92f68aace75b4e0e
Will Yeadon <chvyalthan@gmail.com>
2023-02-02 16:58:49 +0000

    5-7

--- ctci/chpt5/problem-5-7.py
+++ ctci/chpt5/problem-5-7.py
@@ -1,9 +1,30 @@
-def bitSwap(x):
-    pass
+def bitSwap(x):    
+    # Gonna have to manipulate as string
+    orig = bin(x).split('0b')[1][::-1]
+    if (len(orig) % 2) == 1:
+        orig += '0'
+    
+    odds = ''
+    evens = ''
+    
+    for i in range(0,len(orig), 2):
+        evens += orig[i]
+    
+    for i in range(1, len(orig), 2):
+        odds += orig[i]
+    
+    output = ''
+    for i, j in zip(odds, evens):
+        output += i
+        output += j
+        
+    
+    return '0b' + output[::-1]
 
-a = bin(1)
-b = bin(5)
-c = bin(25)
-d = bin(40)
+a = 25
+b = 40
+c = 100
 
-print(a,b,c,d)
+print(bin(a), bitSwap(a))
+print(bin(b), bitSwap(b))
+print(bin(c), bitSwap(c))
