919e2226b1bacd663624ea71041ae69f309a6278
Will Yeadon <chvyalthan@gmail.com>
2023-02-08 22:42:20 +0000

    8-5

---
+++ ctci/chpt8/problem-8-5.py
@@ -0,0 +1,11 @@
+# Recursive multiplier without using *
+
+def recursiveMult(x, y):
+    if y == 0:
+        return 0
+    else:
+        return x + recursiveMult(x, y - 1)
+
+print(recursiveMult(5, 5))
+print(recursiveMult(6, 7))
+print(recursiveMult(3, 10))
