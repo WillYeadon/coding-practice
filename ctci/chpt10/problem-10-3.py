d6a6d43ce31e9bd766c80dffe3175a5d8b8cae3e
Will Yeadon <chvyalthan@gmail.com>
2023-02-08 19:53:52 +0000

    10-3

--- ctci/chpt10/problem-10-3.py
+++ ctci/chpt10/problem-10-3.py
@@ -18,8 +18,8 @@ def builtInFindN(x, N):
     
     print(N, 'is at index', x.index(N))
     
-builtInFindN(x, 3)
-builtInFindN(x, 7)
+#builtInFindN(x, 3)
+#builtInFindN(x, 7)
 
 def search(arr, target):
     # initialize the start and end indices
@@ -31,26 +31,35 @@ def search(arr, target):
         mid = (left + right) // 2
         
         # check if the middle element is equal to the target
+        # If so exit here
         if arr[mid] == target:
             return mid
         
-        # if the right side of the array is sorted
+        # Check if the right side of the array is sorted
         elif arr[mid] < arr[right]:
-            # check if the target is present in the right side
+            # If it is, check if the target is present in 
+            # the right side
             if target > arr[mid] and target <= arr[right]:
                 left = mid + 1
+            # If it isn't (and the right side is sorted) then
+            # must be in left side so set right to be one to
+            # the left of mid
             else:
                 right = mid - 1
         
-        # if the left side of the array is sorted
+        # One of the sides has to be sorted so if the right
+        # side is not sort the left side is
         else:
-            # check if the target is present in the left side
+            # Check if the target is present in the left side
+            # The left side is sorted in this situation
             if target >= arr[left] and target < arr[mid]:
                 right = mid - 1
+            # If not in sorted left side then shift left one to
+            # the right of mid
             else:
                 left = mid + 1
     
     # return -1 if the target is not found
     return -1
 
-print(search(x, 3))
+print('3 is in position,', search(x, 3))
