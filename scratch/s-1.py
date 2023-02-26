36260a706a95a1e6080285b2114cde1deee8a789
Will Yeadon <chvyalthan@gmail.com>
2023-02-26 17:36:41 +0000

    98

--- scratch/s-1.py
+++ scratch/s-1.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Node:
     def __init__(self, val):
         self.val = val
@@ -5,10 +7,26 @@ class Node:
         self.right = None
 
 def traverse(root, func):
+    # If not at end
     if root:
+        # Call left repeatedly until at end
         traverse(root.left, func)
+        # Apply func
         func(root)
+        # Call right
         traverse(root.right, func)
+
+def traverseBFS(root, func):
+    if not root:
+        return
+    queue = deque([root])
+    while queue:
+        node = queue.popleft()
+        func(node)
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
 
 def check_gt_5(node):
     if node.val > 5:
@@ -23,3 +41,4 @@ root.right.left = Node(5)
 root.right.right = Node(7)
 
 traverse(root, check_gt_5)
+traverseBFS(root, check_gt_5)
