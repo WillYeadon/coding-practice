e1fd5191507351bd01ccfe8547e6f5e1dd31524d
Will Yeadon <chvyalthan@gmail.com>
2023-02-26 16:53:46 +0000

    valid-bst

---
+++ leetcode/py/98-Validate-BST.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:  
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def helper(node, lower=float('-inf'), upper=float('inf')):
+            if not node:
+                return True
+            val = node.val
+            if val <= lower or val >= upper:
+                return False
+            if not helper(node.right, val, upper):
+                return False
+            if not helper(node.left, lower, val):
+                return False
+            return True
+
+        return helper(root)
+    
+root = TreeNode(4)
+root.left = TreeNode(2)
+root.right = TreeNode(6)
+root.left.left = TreeNode(1)
+root.left.right = TreeNode(3)
+root.right.left = TreeNode(5)
+root.right.right = TreeNode(7)
+
+x = Solution()
+print(x.isValidBST(root))
