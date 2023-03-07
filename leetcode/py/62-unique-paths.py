654d9855d4244cd048eca9638385818052e82861
Will Yeadon <chvyalthan@gmail.com>
2023-03-07 19:11:13 +0000

    62

--- leetcode/py/62-unique-paths.py
+++ leetcode/py/62-unique-paths.py
@@ -1,10 +1,16 @@
+from math import factorial
+
 class SolutionA:
     def uniquePaths(self, m: int, n: int) -> int:
         def helper(m, n, x=0, y=0):
+            # It is only a path if you have reached x = m-1 and
+            # y = n-1 at the end of the recursion stack
             if x == m-1 and y == n-1:
                 return 1
+            # Out of bounds so noth a path
             if x >= m or y >= n:
                 return 0
+            # Return the total of going right (x+1) and down (y+1)
             return helper(m, n, x+1, y) + helper(m, n, x, y+1)
 
         return helper(m, n)
@@ -36,12 +42,19 @@ class SolutionB:
         # Call the helper function with the starting coordinates (0,0)
         return helper(0, 0)
 
+class SolutionC:
+    # There is a third combinatorics method. Although this
+    # is very difficult to work out without knowing it in advance
+    def uniquePaths(self, m: int, n: int) -> int:      
+        return factorial((m-1) + (n-1)) // (factorial(m-1)* factorial(n-1))
 
     
 x = SolutionA()
-print(x.uniquePaths(3, 7))
-print(x.uniquePaths(3, 2))
+print(x.uniquePaths(3, 7), x.uniquePaths(3, 2))
 
 y = SolutionB()
-print(y.uniquePaths(3, 7))
-print(y.uniquePaths(3, 2))
+print(y.uniquePaths(3, 7), y.uniquePaths(3, 2))
+
+z = SolutionC()
+print(z.uniquePaths(3, 7), z.uniquePaths(3, 2))
+
