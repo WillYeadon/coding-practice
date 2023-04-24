04ce8535f045eb7207684af3eccfb5a33c523557
Will Yeadon <chvyalthan@gmail.com>
2023-04-24 18:23:58 +0100

    7

--- leetcode/js/7-reverse-integer.js
+++ leetcode/js/7-reverse-integer.js
@@ -3,7 +3,38 @@
  * @return {number}
  */
 var reverse = function(x) {
-    
+    let negative = false;
+
+    if (x < 0) {
+        negative = true;
+        x = -x;
+    }
+
+    ans = 0;
+    digits = x.toString().split('').map(Number);
+
+    // Remove leading/trailing zeros.
+
+    while (digits[digits.length - 1] === 0) {
+        digits.pop();
+    }
+
+    var reversed = [];
+    while (digits.length > 0) {
+        reversed.push(digits.pop());
+    }
+
+    ans = reversed.reduce((acc, val) => acc * 10 + val, 0);
+
+    if (negative) {
+        ans = -ans;
+    }
+
+    if ((ans < -2147483648) || (ans > 2147483647)) {
+        return 0;
+    }
+
+    return ans;
 };
 
 // 321
