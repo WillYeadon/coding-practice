ef9e9b593e04da3e93ad6f59ceba066a9c93904f
Will Yeadon <chvyalthan@gmail.com>
2023-07-02 20:10:17 +0100

    274

---
+++ leetcode/js/274-h-index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function(citations) {
+    citations.sort((a, b) => a - b);
+        
+    let i = 0;
+    let h = citations[i];
+
+    while (i < citations.length - 1 && h < citations.slice(i + 1).length) {
+        i += 1;
+        h = citations[i];
+    }
+
+    return Math.min(h, citations.length - i);
+};
