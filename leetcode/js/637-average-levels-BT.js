0dbf484312e7438deb0bc893b31f6bf506b77806
Will Yeadon <chvyalthan@gmail.com>
2023-07-17 11:11:28 +0100

    637

---
+++ leetcode/js/637-average-levels-BT.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function(root) {
+    if (!root) return [];  // Check if the root is not null
+
+    let queue = [root];
+    let averages = []
+
+    while (queue.length > 0) {
+        let size = queue.length;
+        let sum = 0;
+
+        for (let i = 0; i < size; i++) {
+            let node = queue.shift();
+            sum += node.val;
+
+            if (node.left) {
+                queue.push(node.left);
+            }
+
+            if (node.right) {
+                queue.push(node.right);
+            }
+        }
+
+        averages.push(sum / size);
+    }
+
+    return averages;
+};
