46beccf5e09639f3e4e97d0baa9e5c058e3dccc6
Will Yeadon <chvyalthan@gmail.com>
2023-09-21 21:26:23 +0100

    373

---
+++ leetcode/py/373-k-smallest.py
@@ -0,0 +1,23 @@
+import heapq
+from typing import List
+
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        if not nums1 or not nums2:
+            return []
+        
+        heap = []
+        for i in range(min(k, len(nums1))):
+            heapq.heappush(heap, (nums1[i] + nums2[0], i, 0))
+        
+        ans = []
+        while k > 0 and heap:
+            val, i, j = heapq.heappop(heap)
+            ans.append([nums1[i], nums2[j]])
+
+            if j + 1 < len(nums2):
+                heapq.heappush(heap, (nums1[i] + nums2[j + 1], i, j + 1))
+            
+            k -= 1
+        
+        return ans
