ad0fa0e07f0b4fb23148c3c2e225dcfdbd171d58
Will Yeadon <chvyalthan@gmail.com>
2023-12-07 20:39:37 +0000

    61

---
+++ leetcode/py/61-rotate-ll.py
@@ -0,0 +1,26 @@
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or k < 1:
+            return head
+        length = 1
+        last_node = head
+        while last_node.next:
+            last_node = last_node.next
+            length += 1
+
+        last_node.next = head
+        k = k % length
+        steps_to_new_head = length - k
+        new_tail = head
+        for _ in range(steps_to_new_head - 1):
+            new_tail = new_tail.next
+
+        new_head = new_tail.next
+        new_tail.next = None
